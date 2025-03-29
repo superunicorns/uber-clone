e0e8d924bed06e42b9259c3056bc6711a746e66d
UnicornDad8 <unicorndad8@gmail.com>
2025-03-29 15:29:51 -0300

    Configure eslint and prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,11 @@
-import { View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../../global.css";
-
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="items-center justify-center flex-1 bg-white">
       <Text className="text-red-500">Hello World</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
